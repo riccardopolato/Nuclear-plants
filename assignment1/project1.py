2eab2c5945bd9e5426eee59b6082f7c3c04d015d
riccardopolato <s359217@studenti.polito.it>
2026-03-02 16:56:25 +0100

    calcolo area e portata

--- assignment1/project1.py
+++ assignment1/project1.py
@@ -3,6 +3,51 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
+def get_pipe_geometry(outside_diameter_inch, thickness_inch):
+    """
+    Calcola diametro interno e area a partire dalle dimensioni nominali in pollici.
+    Restituisce i valori in metri e metri quadri.
+    """
+    # 1. Calcolo del diametro interno in pollici (OD - 2*t)
+    # 2. Conversione immediata in metri (* 0.0254)
+    d_inner_m = (outside_diameter_inch - 2 * thickness_inch) * 0.0254
+    
+    # 3. Calcolo dell'area della sezione trasversale (A = pi * r^2)
+    area_m2 = np.pi * (d_inner_m / 2)**2
+    
+    return d_inner_m, area_m2
+
+def calculate_massflow_and_velocity(Q_total, p, D):
+    # Calcola la portata massica e le velocità del liquido e del vapore
+    # Entalpia del liquido saturo (inizio ebollizione)
+    h_liq = CP.PropsSI('H', 'P', p, 'Q', 0, 'Water')
+
+    # Entalpia del vapore saturo (fine ebollizione)
+    h_vap = CP.PropsSI('H', 'P', p, 'Q', 1, 'Water')
+
+    # Salto entalpico di evaporazione (Calore latente)
+    delta_h_evap = h_vap - h_liq
+
+    # Portata massica necessaria per far evaporare tutto il fluido
+    m_dot = Q_total / delta_h_evap
+
+    # Area della sezione trasversale del tubo
+    A = np.pi * (D / 2) ** 2
+
+    # Velocità del liquido
+    v_liq = m_dot / (CP.PropsSI('D', 'P', p, 'Q', 0, 'Water') * A)
+
+    # Velocità del vapore
+    v_vap = m_dot / (CP.PropsSI('D', 'P', p, 'Q', 1, 'Water') * A)
+
+    return m_dot, v_liq, v_vap
+
+
+
+
+
 # datas
 p = 70 *10**5  # Pa
 Q_thermal = 34.8 * 10**6  # W
@@ -14,41 +59,7 @@ k_c = 20
 # Outside diameter - thickness (STD)
 Outside_diameter = 1.9  # inches
 thickness = 0.145  # inches  
-D, A = calculate_diameter_and_area(Outside_diameter, thickness)
-
-def calculate_diameter_and_area(outside_diameter, thickness):
-    # Calcola il diametro in metri e l'area della sezione trasversale del tubo
-    # Formula diametro: (Diametro esterno - Spessore) * Fattore di conversione (0.0254)
-    D = (outside_diameter - 2 * thickness) * 0.0254
-    # Formula area: π * (D / 2)^2
-    A = np.pi * (D / 2) ** 2
-    return D, A
-
-#ricavo le velocità
-# def calculate_massflow_and_velocity(Q_total, p, D):
-#     # Calcola la portata massica e le velocità del liquido e del vapore
-#     # Entalpia del liquido saturo (inizio ebollizione)
-#     h_liq = CP.PropsSI('H', 'P', p, 'Q', 0, 'Water')
-
-#     # Entalpia del vapore saturo (fine ebollizione)
-#     h_vap = CP.PropsSI('H', 'P', p, 'Q', 1, 'Water')
-
-#     # Salto entalpico di evaporazione (Calore latente)
-#     delta_h_evap = h_vap - h_liq
-
-#     # Portata massica necessaria per far evaporare tutto il fluido
-#     m_dot = Q_total / delta_h_evap
-
-#     # Area della sezione trasversale del tubo
-#     A = np.pi * (D / 2) ** 2
-
-#     # Velocità del liquido
-#     v_liq = m_dot / (CP.PropsSI('D', 'P', p, 'Q', 0, 'Water') * A)
-
-#     # Velocità del vapore
-#     v_vap = m_dot / (CP.PropsSI('D', 'P', p, 'Q', 1, 'Water') * A)
-
-#     return m_dot, v_liq, v_vap
+D = calculate_diameter(Outside_diameter, thickness)
 
 # Esempio di utilizzo della funzione
-# m_dot, v_liq, v_vap = calculate_massflow_and_velocity(Q_thermal, p, D)
+m_dot, v_liq, v_vap = calculate_massflow_and_velocity(Q_thermal, p, D)
